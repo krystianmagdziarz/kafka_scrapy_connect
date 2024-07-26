3d47819d3f74a5834c25c99d8a94ae15662255d3
Krystian Magdziarz <48068513+krystianmagdziarz@users.noreply.github.com>
2024-07-26 22:07:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README_MD = open(os.path.join(MY_DIR, 'README.md')).read()
 
 setup(
     name='kafka-scrapy-connect',
-    version='{{VERSION_PLACEHOLDER}}',
+    version='2.5.1',
     description='Integrating Scrapy with kafka using the confluent-kafka python client',
     long_description=README_MD,
     long_description_content_type="text/markdown",
